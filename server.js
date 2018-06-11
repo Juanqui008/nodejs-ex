ec6275c62bb8dc1acc141365a8e9641eacddf8dc
Juan Carlos Zaragoza Tomás <zaragoza007@hotmail.com>
2018-06-11 13:15:35 +0200

    initial commit

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const bot = new TelegramBot(token, {polling: true});
  bot.on('message', (msg) => {
  var Hola = "hola";
  if (msg.text.toString().toLowerCase().indexOf(Hola) === 0) {
-     bot.sendMessage(msg.chat.id, "Hola  " + msg.from.first_name);
+     bot.sendMessage(msg.chat.id, "Hola" + msg.from.first_name);
  }
  });
  
  bot.onText(/\Lista de comandos/, (msg) => {
- bot.sendMessage(msg.chat.id, "Hola, Córtate, córtate, Is that allowed?, Shut up, Hurricane Katrina, A quién le estás diciendo shut up, Chillin in the hot tub, Pussy, Hostión");    
+ bot.sendMessage(msg.chat.id, "Hola, Hola Josemi, Córtate, córtate, Is that allowed?, Shut up, Hurricane Katrina, A quién le estás diciendo shut up, Chillin in the hot tub, Pussy, Hostión, I love u bitch, Fuck off, Thanks, I´m calling the police, ");    
  });
 
 bot.onText(/\Córtate, córtate/, (msg) => {
@@ -63,6 +63,31 @@ bot.onText(/\Pussy/, (msg) => {
 
 bot.onText(/\Hostión/, (msg) => {
    const url = 'https://ia601503.us.archive.org/11/items/ComeGetYourJuice/Come%20Get%20Your%20Juice.mp3';
+   const audio = request(url);
+   bot.sendVoice(msg.chat.id, audio);  
+  });
+bot.onText(/\I love u bitch/, (msg) => {
+   const url = 'https://ia601507.us.archive.org/27/items/ILoveYouBitch.VINE96kbitVorbis/I%20love%20you%2C%20bitch.%20VINE%20%2896kbit_Vorbis%29.ogg';
+   const audio = request(url);
+   bot.sendVoice(msg.chat.id, audio);  
+  });
+bot.onText(/\Fuck off/, (msg) => {
+   const url = 'https://ia601501.us.archive.org/0/items/FuckOffJanetImNotGoingToYourFuckingBabyShowerOriginalVine128kbitAAC_201806/Fuck%20off%20janet%20I%E2%80%99m%20not%20going%20to%20your%20fucking%20baby%20shower%20original%20vine%20%28128kbit_AAC%29.mp3';
+   const audio = request(url);
+   bot.sendVoice(msg.chat.id, audio);  
+  });
+bot.onText(/\Thanks/, (msg) => {
+   const url = 'https://ia601506.us.archive.org/33/items/ItsAnAvocado....thanks....96kbitAAC/It%27s%20an%20avocado%21....thanks%21....%20%2896kbit_AAC%29.mp3';
+   const audio = request(url);
+   bot.sendVoice(msg.chat.id, audio);  
+  });
+bot.onText(/\I´m calling the police/, (msg) => {
+   const url = 'https://ia601508.us.archive.org/20/items/ImCallingThePolice128kbitAAC/I%27m%20calling%20the%20police%20%28128kbit_AAC%29.mp3';
+   const audio = request(url);
+   bot.sendVoice(msg.chat.id, audio);  
+  });
+bot.onText(/\Hola Josemi/, (msg) => {
+   const url = 'https://ia601505.us.archive.org/35/items/HolaJosemiVioladorDeMierda192kbitAAC/Hola%20Josemi%20violador%20de%20Mierda%20%28192kbit_AAC%29.mp3';
    const audio = request(url);
    bot.sendVoice(msg.chat.id, audio);  
   });
